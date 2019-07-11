b88e09b1b30911fb2ffab1338231a81e8dd57e93
Andrew Kloc <Andrew.Kloc@liazon.com>
2019-07-10 21:58:32 -0400

    Chapter 2 done.

---
+++ Chapter2/favorite_number.py
@@ -0,0 +1,7 @@
+# Fav number is 23.
+
+favorite_number = 23
+
+favorite_number_message = "My favorite number is, you guessed it... " + str(favorite_number)
+
+print(favorite_number_message)
---
+++ Chapter2/number_8.py
@@ -0,0 +1,7 @@
+print(4 + 4)
+
+print(16 / 4)
+
+print(4 * 1)
+
+print(6 - 2)
